3e66a968816c0d51a16aa6a3c7556ea69581435c
Gustavo Lesna <gustavo@example.com>
2025-08-19 13:11:41 -0300

    Segundo

---
+++ LogisticaApp.Data/IProductoRepository.cs
@@ -0,0 +1,18 @@
+﻿using LogisticaApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticaApp.Data
+{
+    public interface IProductoRepository
+    {
+        Task<IEnumerable<Producto>> GetAll();
+        Task<Producto> GetById(int id);
+        Task Add(Producto producto);
+        Task Update(Producto producto);
+        Task Delete(int id);
+    }
+}
---
+++ LogisticaApp.Data/ProductoRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using LogisticaApp.Entities;
+
+using System.Data;
+
+
+namespace LogisticaApp.Data
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly IDbConnection _db;
+
+        public ProductoRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<Producto>> GetAll() =>
+            _db.QueryAsync<Producto>("SELECT * FROM Productos;");
+
+        public Task<Producto> GetById(int id) =>
+            _db.QueryFirstOrDefaultAsync<Producto>("SELECT * FROM Productos WHERE Id=@Id;", new { Id = id });
+
+        public Task Add(Producto producto) =>
+            _db.ExecuteAsync("INSERT INTO Productos (Nombre,Cantidad,Precio,Ubicacion) VALUES (@Nombre,@Cantidad,@Precio,@Ubicacion);", producto);
+
+        public Task Update(Producto producto) =>
+            _db.ExecuteAsync("UPDATE Productos SET Nombre=@Nombre,Cantidad=@Cantidad,Precio=@Precio,Ubicacion=@Ubicacion WHERE Id=@Id;", producto);
+
+        public Task Delete(int id) =>
+            _db.ExecuteAsync("DELETE FROM Productos WHERE Id=@Id;", new { Id = id });
+    }
+}
